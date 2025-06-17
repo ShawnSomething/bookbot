309ee4472aba09042e02402a4ec950fe980836f7
ShawnSomething <khooweishawn@gmail.com>
2025-06-18 07:26:35 +1000

    more cleaning

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from stats import get_book_text, count_words, count_characters, sorts
 
+file_path = "books/frankenstein.txt"
+
 def main():
-    count_words("books/frankenstein.txt") 
-    sorts("books/frankenstein.txt")
+    count_words(file_path) 
+    sorts(file_path)
 
 
 main()
